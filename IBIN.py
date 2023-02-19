e753bae1b5f5e2bcd3c14fedbf189d440254ba80
TheFrechTechMan <massardier.camille@gmail.com>
2023-02-19 13:14:31 +0100

    obfuscated a little

--- IBIN.py
+++ IBIN.py
@@ -1,37 +1,18 @@
 j = 0
-
-inputstring = input("Enter a string to encode/decode with IBIN (Inverted-BINary): ")
-
-MAX = 1114111
- 
-def encrypt(string: str) -> str:
-     max_char_code = MAX
-     return ' '.join([format(ord(char) ^ max_char_code, 'b')
-                      for char in string])
- 
-def decrypt(string: str) -> str:
-     max_char_code = MAX
-     return ''.join([chr(int(char, 2) ^ max_char_code)
-                     for char in string.split(' ')])
- 
-bstr = encrypt(inputstring)
-
-i = len(bstr)
-
-def binary_to_dec(bin: str) -> int:
-    return int(bin, 2)
-
-def convert_binary_to_char(bin: str) -> str:
-    return chr(binary_to_dec(bin))
-
-def convert_binary_with_space_to_string(bin: str) -> str:
-    convert = bin.split(' ')
-    result = []
-
-    for c in convert:
-        result.append(convert_binary_to_char(c))
-    return ''.join(result)
-
-festr = convert_binary_with_space_to_string(bstr)
-
-print("Your encoded/decoded string = " + festr + " (don't worry if the characters are not recognized).")
+b = 1114111
+def ed(c: str) -> str:
+     a = b
+     return ' '.join([format(ord(char) ^ a, 'b')
+                      for char in c])
+def btd(d: str) -> int:
+    return int(d, 2)
+def btc(d: str) -> str:
+    return chr(btd(d))
+def bwsts(d: str) -> str:
+    e = d.split(' ')
+    r = []
+    for c in e:
+        r.append(btc(c))
+    return ''.join(r)
+def encode_decode(s):
+    return bwsts(ed(s))
